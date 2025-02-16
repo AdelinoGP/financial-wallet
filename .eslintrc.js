2276899a9b796fa39e47b1ed2a4d5fc0ee3d2117
Adelino Penedo <agpene@hotmail.com>
2025-02-16 19:56:33 -0300

    Ported linter settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: 12,
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
@@ -12,6 +13,7 @@ module.exports = {
   ],
   root: true,
   env: {
+    es2021: true,
     node: true,
     jest: true,
   },
@@ -20,6 +22,40 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    "@typescript-eslint/no-extra-semi": "off",
+    "@typescript-eslint/member-delimiter-style": "off",
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "_",
+      },
+    ],
+    semi: "off",
+
+    quotes: ["error", "double", { avoidEscape: true }],
+    "prefer-const": "error",
+    "space-before-function-paren": [
+      "error", {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always",
+      },
+    ],
+    "node/no-callback-literal": "off",
+    curly: ["off", "multi"],
+    "array-bracket-newline": ["error", { multiline: true }],
+    "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
+    "no-multiple-empty-lines": "off",
+
+    "require-await": "error",
+    "no-mixed-operators": "off",
+
+    eqeqeq: ["off", "always"],
+    "comma-dangle": ["off", "always"],
+    "no-unused-expressions": "error",
+
+    "spaced-comment": ["error", "never"],
   },
 };
